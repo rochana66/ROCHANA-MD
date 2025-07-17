07c59178539df20ab94755d47ff8c670e9ff8415
Rochana99 <116473568+Rochana99@users.noreply.github.com>
2025-07-17 17:29:16 +0530

    ♡ 𝗥𝗢𝗖𝗛𝗔𝗡𝗔-𝗠𝗗 😽💘✨

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~7N03EApA#L9OUfOYLt9GoKU_3fV734ljBaQbuEk0G0dmzpZ02pss",
+SESSION_ID: process.env.SESSION_ID || "385XRZ6Q#uTOERGWwY2J0qOT9yBmi4RXTbP3STn3GNHkg-3z1Vo8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
