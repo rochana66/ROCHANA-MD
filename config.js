fa7645dc33986ba04932c2d8be18a0e1e5babc15
rochana66 <bandarajandeepa@gmail.com>
2025-07-18 08:20:50 +0530

    ♡ 𝗥𝗢𝗖𝗛𝗔𝗡𝗔-𝗠𝗗 😽💘✨

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hw4yRKRD#TPoVBtSdB48qnBP4zgqbyrmgU8HVjddNt4KSyKDcjsE",
+SESSION_ID: process.env.SESSION_ID || "rochana~hw4yRKRD#TPoVBtSdB48qnBP4zgqbyrmgU8HVjddNt4KSyKDcjsE",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
